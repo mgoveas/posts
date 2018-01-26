7f51e4abe3247bb51009f8eb113197c5993b0d19
Maria Goveas <Maria_Goveas@intuit.com>
2018-01-26 09:16:04 -0800

    course work

---
+++ src/components/posts_new.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+import {createPost} from '../actions';
+
+class PostsNew extends Component{
+
+  renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? "has-danger" : ""}`;
+    return (
+      <div className={className}>
+        <label>{field.label}</label>
+        <input className="form-control" type="text" {...field.input} />
+        <div className="text-help">
+          {touched ? error : ""}
+        </div>
+      </div>
+    );
+  }
+
+  onSubmit(values){
+    console.log(values);
+    this.props.createPost(values, () => {
+      this.props.history.push("/");
+    });
+  }
+
+  render(){
+    const {handleSubmit} = this.props;
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field name="title" component={this.renderField} label="Title"/>
+        <Field name="categories" component={this.renderField} label="Categories"/>
+        <Field name="content" component={this.renderField} label="Content"/>
+        <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
+      </form>
+    );
+  }
+}
+
+function validate(values) {
+  const errors = {};
+  // Validate the inputs from 'values'
+  if (!values.title) {
+    errors.title = "Enter a title";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter some categories";
+  }
+  if (!values.content) {
+    errors.content = "Enter some content please";
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+})(connect(null, {createPost})(PostsNew));
